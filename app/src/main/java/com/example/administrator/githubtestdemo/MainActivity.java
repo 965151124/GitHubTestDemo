f0cc485754d993e72d8f47f3cc4dbd3e8ec4c8cb
Administrator <15011131966@163.com>
2016-03-11 16:25:50 +0800

    这是我第一次修改的地方

--- app/src/main/java/com/example/administrator/githubtestdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubtestdemo/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这是我修改的地方
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
